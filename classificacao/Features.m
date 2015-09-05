4d4dc578fd73419e3b81deabbf7db0af670a15fa
Fernanda Bernardo <fernandabernardo94@gmail.com>
2015-09-04 23:49:21 -0300

    refactoring code for bag of features

--- classificacao/Features.m
+++ classificacao/Features.m
@@ -5,6 +5,10 @@ classdef Features
 		numberOfImages
 		classification
 	end
+	properties(Constant)
+		imgSize = 50
+		numberOfClusters = 5
+	end
 
 	methods
 		function obj = Features(mapClass, mapNumberOfImages)
@@ -15,13 +19,13 @@ classdef Features
 			obj.classification = cell2mat(values(mapClass));
 		end
 
-		function [] = extract(obj, typeFeature)
-			for position=1:obj.numberOfClasses
+		function [] = extract(this, typeFeature)
+			for position=1:this.numberOfClasses
 				disp(strcat('class: ', position, ' typeFeature: ', typeFeature));
 
-				for file = 1:obj.numberOfImages(position)
-					imgcolor = imread(strcat(obj.namespaces(position,:), num2str(file), '.jpg'));
-				    imgcolor = imresize(imgcolor, [50,50]);
+				for file = 1:this.numberOfImages(position)
+					imgcolor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+				    imgcolor = imresize(imgcolor, [this.imgSize, this.imgSize]);
 				    img = rgb2gray(imgcolor);
 				    [h, w] = size(img);
 
@@ -42,16 +46,16 @@ classdef Features
 						end
 				    end
 
-				    saveFile(typeFeature, feature, obj.classification(position));
+				    saveFile(typeFeature, feature, this.classification(position));
 				end
 			end
 		end
 
-		function [] = extractEntropy(obj)
-			for position=1:obj.numberOfClasses
-				for file = 1:obj.numberOfImages(position)
-					imgcolor = imread(strcat(obj.namespaces(position,:), num2str(file), '.jpg'));
-				    imgcolor = imresize(imgcolor, [50,50]);
+		function [] = extractEntropy(this)
+			for position=1:this.numberOfClasses
+				for file = 1:this.numberOfImages(position)
+					imgcolor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+				    imgcolor = imresize(imgcolor, [this.imgSize, this.imgSize]);
 				    img = rgb2gray(imgcolor);
 				    [h, w] = size(img);
 
@@ -69,9 +73,213 @@ classdef Features
 				       	end
 				    end
 
-				    saveFile('Entropy', entropia, obj.classification(position));
+				    saveFile('Entropy', entropia, this.classification(position));
 				end
 			end
+		end
+
+		function [] = extractOpponentColorSpace(this)
+			for position=1:this.numberOfClasses
+				for file = 1:this.numberOfImages(position)
+					allCenters = [];
+
+					imgColor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+					imgColor = imresize(imgColor, [this.imgSize this.imgSize]);
+
+					% pre process image with formulas from article
+					obj = PreProcessing(imgColor);
+
+					%%%%%%%%%%%%% OPPONENT COLOR SPACE 1 %%%%%%%%%%%%%
+					processedImage = obj.opponentColorSpace1;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+
+					%%%%%%%%%%%%% OPPONENT COLOR SPACE 2 %%%%%%%%%%%%%
+					processedImage = obj.opponentColorSpace2;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+
+					%%%%%%%%%%%%% OPPONENT COLOR SPACE 3 %%%%%%%%%%%%%
+					processedImage = obj.opponentColorSpace3;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+					saveFile('opponentColorSpace', allCenters, this.classification(position));
+				end
+			end
+		end
+
+		function [] = extractNormalizedRG(this)
+			for position=1:this.numberOfClasses
+				for file = 1:this.numberOfImages(position)
+					allCenters = [];
+
+					imgColor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+					imgColor = imresize(imgColor, [this.imgSize this.imgSize]);
+
+					% pre process image with formulas from article
+					obj = PreProcessing(imgColor);
+
+					%%%%%%%%%%%%% NORMALIZED R %%%%%%%%%%%%%
+					processedImage = obj.normalizedR;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+
+					%%%%%%%%%%%%% NORMALIZED G %%%%%%%%%%%%%
+					processedImage = obj.normalizedG;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+					saveFile('normalizedRG', allCenters, this.classification(position));
+				end
+			end
+		end
+
+		function [] = extractHUE(this)
+			for position=1:this.numberOfClasses
+				for file = 1:this.numberOfImages(position)
+					allCenters = [];
+
+					imgColor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+					imgColor = imresize(imgColor, [this.imgSize this.imgSize]);
+
+					% pre process image with formulas from article
+					obj = PreProcessing(imgColor);
+					processedImage = obj.hue;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+					saveFile('hue', allCenters, this.classification(position));
+				end
+			end
+		end
+
+		function [] = extractTransformedRGB(this)
+			for position=1:this.numberOfClasses
+				for file = 1:this.numberOfImages(position)
+					allCenters = [];
+
+					imgColor = imread(strcat(this.namespaces(position,:), num2str(file), '.jpg'));
+					imgColor = imresize(imgColor, [this.imgSize this.imgSize]);
+
+					% pre process image with formulas from article
+					obj = PreProcessing(imgColor);
+
+					%%%%%%%%%%%%% TRANSFORMED R %%%%%%%%%%%%%
+					processedImage = obj.transformedR;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+
+					%%%%%%%%%%%%% TRANSFORMED G %%%%%%%%%%%%%
+					processedImage = obj.transformedG;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+
+					%%%%%%%%%%%%% TRANSFORMED B %%%%%%%%%%%%%
+					processedImage = obj.transformedB;
+
+					features = this.extractBagOfFeatures(processedImage);
+
+					% organize array of features to cluster
+					[h w] = size(features);
+					f = reshape(features', [h*w 1]);
+					[idx, centers] = kmeans(f, this.numberOfClusters);
+					allCenters = [allCenters; centers];
+
+					saveFile('transformedRGB', allCenters, this.classification(position));
+				end
+			end
+		end
+
+		function [features] = extractBagOfFeatures(this, processedImage)
+			features = [];
+			v = Verification;
+
+			% apply gradient on image
+			[mag dir] = imgradient(processedImage);
+
+			% extract key points from image
+			points = detectFASTFeatures(mag);
+
+			location = v.verifyKeyPoints(points.Location, this.numberOfClusters);
+			[h w] = size(location);
+			%image(mag); hold on;
+
+			% on each key point...
+			for i=1:h
+				% ...get coordinates from key point
+				x = location(i, 1); 
+				y = location(i, 2); 
+			
+				[xi,xf,yi,yf] = v.verifyBorder(x, y, this.imgSize);
+
+				% ...apply a square 16x16
+				region = mag(xi:xf, yi:yf);
+				%[h w] = size(region);
+				
+				%rectangle('Position',[xi yi w h], 'LineWidth',2, 'EdgeColor','r');
+
+				% ...get histogram 8 bins with region that contains gradient
+				[histElements histCenters] = hist(region, 8);
+
+				[h w] = size(histElements);
+				elements = reshape(histElements', [1 h*w]);
+
+				features = [features; elements];
+			end		
 		end
 	end
 end
--- classificacao/caracteristicas.m
+++ classificacao/caracteristicas.m
@@ -2,16 +2,21 @@
 %1-A / 2-B / 3-F / 4-M / 5-P / 6-Fundo
 
 clear;clc;
-keySet = {'Alimentos\recorte_alimento\A', 'Alimentos\recorte_alimento\B', 'Alimentos\recorte_alimento\F', 'Alimentos\recorte_alimento\M', 'Alimentos\recorte_alimento\P', 'Alimentos\recorte_fundo\F'};
-valuesClass = {1,2,3,4,5,6};
-valuesNumberOfImages = {10,10,10,10,10,60};
+keySet = {'Alimentos\recorte_alimento\A', 'Alimentos\recorte_fundo\F'};
+valuesClass = {1,6};
+valuesNumberOfImages = {10,60};
 
 mapClass = containers.Map(keySet, valuesClass);
 mapNumberOfImages = containers.Map(keySet, valuesNumberOfImages);
 
 obj = Features(mapClass, mapNumberOfImages);
-obj.extractEntropy()
-obj.extract('Contrast');
-obj.extract('Correlation');
-obj.extract('Energy');
-obj.extract('Homogeneity');
+%obj.extractEntropy()
+%obj.extract('Contrast');
+%obj.extract('Correlation');
+%obj.extract('Energy');
+%obj.extract('Homogeneity');
+
+%obj.extractOpponentColorSpace();
+%obj.extractNormalizedRG();
+%obj.extractHUE();
+%obj.extractTransformedRGB();
