5f39ed869315eb1ebaa2c41b70115dfe3de1cc6c
Fernanda Bernardo <fernandabernardo94@gmail.com>
2015-09-18 19:10:50 -0300

    adding code for addpath on matlab

---
+++ classificacao/addingpaths.m
@@ -0,0 +1,4 @@
+addpath(genpath('C:\Users\Fernanda\Desktop\ml-sig-pro\MATLAB'));
+addpath(genpath('C:\Users\Fernanda\Desktop\TCC\classificacao\neural network\mlp nova\'));
+addpath(genpath('C:\Users\Fernanda\Desktop\TCC\classificacao\Code Feature Extraction\'));
+addpath(genpath('C:\Users\Fernanda\Desktop\TCC\classificacao\Code Preprocessed\'));
