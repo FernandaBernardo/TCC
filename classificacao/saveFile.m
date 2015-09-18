85d6483855efab49a4be8c6d74c0ab6f86e5b261
Fernanda Bernardo <fernandabernardo94@gmail.com>
2015-09-18 19:07:28 -0300

    removing duplicated code

--- classificacao/saveFile.m
+++
@@ -1,7 +0,0 @@
-function [] = saveFile (prop, values, foodType)
-[h, w] = size(values);
-aux = reshape(values', h*w, 1);
-file = fopen(strcat(prop, '.txt'), 'a');
-fprintf(file, '%6.4f ', aux);
-fprintf(file, '%d\n', foodType);
-fclose(file);
